55567789a3b78bf29ccb8bab690289b079370350
reomashka <romankuvaev820@gmail.com>
2025-07-16 01:28:11 +0300

    feat: The payment system is temporarily unavailable.

--- src/app/MainRoutes.tsx
+++ src/app/MainRoutes.tsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useLocation } from "react-router-dom";
 import { HomePage } from "../pages/HomePage";
 import { ProfilePage } from "../pages/ProfilePage";
 import { HelpPage } from "../pages/HelpPage";
@@ -6,15 +6,23 @@ import { InventoryPage } from "../pages/InventoryPage";
 import { AdminPanelPage } from "../pages/AdminPanelPage";
 import { PrivateRoute } from "./PrivateRoute";
 
-export const MainRoutes = () => (
-  <Routes>
-    <Route path="/" element={<HomePage />} />
-    <Route path="/help" element={<HelpPage />} />
-    <Route path="/profile" element={<ProfilePage />} />
-    <Route path="/inventory" element={<InventoryPage />} />
+export const MainRoutes = () => {
+  const location = useLocation();
+  const state = location.state as { backgroundLocation?: Location };
 
-    <Route element={<PrivateRoute />}>
-      <Route path="/admin" element={<AdminPanelPage />} />
-    </Route>
-  </Routes>
-);
+  return (
+    <Routes
+      location={state?.backgroundLocation || location}
+      key={state?.backgroundLocation ? "main-routes-with-bg" : "main-routes"}
+    >
+      <Route path="/" element={<HomePage />} />
+      <Route path="/help" element={<HelpPage />} />
+      <Route path="/profile" element={<ProfilePage />} />
+      <Route path="/inventory" element={<InventoryPage />} />
+
+      <Route element={<PrivateRoute />}>
+        <Route path="/admin" element={<AdminPanelPage />} />
+      </Route>
+    </Routes>
+  );
+};
--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,27 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
+// import { visualizer } from "rollup-plugin-visualizer";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [
+    react(),
+    tsconfigPaths(),
+    // visualizer({
+    //   filename: "stats.html",
+    //   open: true,
+    //   gzipSize: true,
+    //   brotliSize: true,
+    // }),
+  ],
   server: {
     proxy: {
       "/api": "http://localhost:3000",
       "/uploads": "http://localhost:3000",
     },
   },
+  // build: {
+  //   sourcemap: true,
+  // },
 });
