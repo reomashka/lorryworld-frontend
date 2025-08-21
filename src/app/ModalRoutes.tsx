c602fde536e06667c5ee57799ea7d9ed67a33b54
reomashka <romankuvaev820@gmail.com>
2025-08-21 15:33:54 +0300

    fix: remove unused GameSelection route from ModalRoutes

--- src/app/ModalRoutes.tsx
+++ src/app/ModalRoutes.tsx
@@ -5,7 +5,6 @@ import { ClaimItemsModal } from "../modals/ClaimItemsModal";
 import { RegistrationModal } from "../modals/RegistrationModal";
 import { ItemModal } from "../modals/ItemModal";
 import { TopupModal } from "../modals/TopupModal";
-import { GameSelection } from "@modals/GameSelection";
 
 export const ModalRoutes = () => (
   <Routes>
@@ -15,6 +14,5 @@ export const ModalRoutes = () => (
     <Route path="/register" element={<RegistrationModal />} />
     <Route path="/item" element={<ItemModal />} />
     <Route path="/topup" element={<TopupModal />} />
-    <Route path="/game-selection" element={<GameSelection />} />
   </Routes>
 );
